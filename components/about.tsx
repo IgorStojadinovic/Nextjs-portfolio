aa773eae592febcd5457c31a9c563304aa657c7c
Igor Stojadinovic <igorstojadinovic.vs@gmail.com>
2024-03-10 18:25:11 +0100

    update

--- components/about.tsx
+++ components/about.tsx
@@ -1,50 +1,49 @@
-'use client';
-import React from 'react';
-import SectionHeading from './section-heading';
-import { motion } from 'framer-motion';
-import { useSectionInView } from '@/lib/hooks';
+"use client";
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
 
 const About = () => {
-  const { ref } = useSectionInView('About');
+  const { ref } = useSectionInView("About");
 
   return (
     <motion.section
       ref={ref}
-      className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28'
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
-      id='about'
+      id="about"
     >
       <SectionHeading>About Me</SectionHeading>
-      <p className='mb-3'>
+      <p className="mb-3">
         Few years ago,I decided to requalify and pursue carrier in programming.
-        I enrolled a Python coding bootcamp and over time learned{' '}
-        <span className='font-medium'>full-stack web development</span>.{' '}
-        <span className='italic'>My favorite part of programming</span> is the
-        problem-solving aspect. I <span className='underline'>love</span> the
+        I enrolled a Python coding bootcamp and over time learned{" "}
+        <span className="font-medium">full-stack web development</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
-        is{' '}
-        <span className='font-medium'>
-          React, Next.js, Node.js, and MySQL
-        </span>
-        . I am also familiar with TypeScript. I am always looking to learn new
-        technologies. I am currently looking for a{' '}
-        <span className='font-medium'>full-time position</span> as a React developer
+        is{" "}
+        <span className="font-medium">React, Next.js, Node.js, and MySQL</span>.
+        I am also familiar with TypeScript. I am always looking to learn new
+        technologies. I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a Frontend
+        developer
       </p>
 
       <p>
-        <span className='italic'>{`When I'm not coding`}</span>, I enjoy working
+        <span className="italic">{`When I'm not coding`}</span>, I enjoy working
         out, watching movies, reading, and listening to Huberman Lab. I also
-        enjoy <span className='font-medium'>art</span>.{' '}
-        {`I've been studying at`}{' '}
+        enjoy <span className="font-medium">art</span>.{" "}
+        {`I've been studying at`}{" "}
         <a
-          href=' https://www.wattsatelier.com/'
-          target='_bank'
-          className='underline'
+          href=" https://www.wattsatelier.com/"
+          target="_bank"
+          className="underline"
         >
           Watts Atiler
-        </a>{' '}
+        </a>{" "}
         for several years now.
       </p>
     </motion.section>
