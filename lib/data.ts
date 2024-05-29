2b97ec7d919f3acb5f8cfd1df46dbfc1d800b803
Igor Stojadinovic <igorstojadinovic.vs@gmail.com>
2024-05-29 13:51:49 +0200

    "update"

--- lib/data.ts
+++ lib/data.ts
@@ -48,7 +48,7 @@ export const experiencesData = [
     title: "Studying Period",
     location: "Vrbas",
     description:
-      "In this period I explored both front and backend technologies and learned core concepts and MERN stack.I've tried to apply for internships,but they were basically only for students. I could not give up at this point, I've invested so much time into this. ",
+      "In this period I explored both front and backend technologies, learned core concepts and MERN stack.I've tried to apply for internships,but they were basically only for students. I could not give up at this point, I've invested so much time into this. ",
     icon: React.createElement(LuGraduationCap),
     date: "2020 - 2022",
   },
