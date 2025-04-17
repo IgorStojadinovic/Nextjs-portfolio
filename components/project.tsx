36456ef850cda696d556a6ac9f75dc51782d462b
IgorST <igorstojadinovic.vs@gmail.com>
2025-04-17 21:02:15 +0200

    updated projects

--- components/project.tsx
+++ components/project.tsx
@@ -1,58 +1,42 @@
-'use client';
-import React, { useRef } from 'react';
-import Image from 'next/image';
-import { motion, useScroll, useTransform } from 'framer-motion';
-import { projectsData } from '@/lib/data';
+"use client";
+import React, { useRef } from "react";
+import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { projectsData } from "@/lib/data";
 type ProjectProps = (typeof projectsData)[number];
 
-const Project = ({
-  title,
-  description,
-  tags,
-  imageUrl,
-  projectUrl,
-}: ProjectProps) => {
-  const ref = useRef<HTMLDivElement>(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ['0 1', '1.33 1'],
-  });
-  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
-  return (
-    <motion.div
-      ref={ref}
-      style={{
-        scale: scaleProgress,
-        opacity: opacityProgress,
-      }}
-      className='group mb-3  sm:mb-8 last:mb-0  w-full'
-    >
-      <section className=' group-even:pl-8 rounded-lg  px-12 flex flex-col bg-gray-100  sm:max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]  sm:even:pl-8 hover:bg-gray-200 transition dark:bg-zinc-950 dark:hover:bg-zinc-900/50 dark:border-emerald-900'>
-        <a href={projectUrl} target='_blank'>
-          <div className='pt-4 pb-7  sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]     '>
-            <h3 className='text-2xl font-semibold'>{title}</h3>
-            <p className='mt-2 mb-8 leading-relaxed text-gray-700 dark:text-white'>
-              {description}
-            </p>
-            <ul className='flex  flex-wrap mt-4 gap-2 sm:mt-auto '>
-              {tags.map((tag, index) => (
-                <li
-                  className='bg-black/[0.7] px-3 text-[0.7rem] uppercase tracking-wider text-white rounded-full '
-                  key={index}
-                >
-                  {' '}
-                  {tag}
-                </li>
-              ))}
-            </ul>
-          </div>
+const Project = ({ title, description, tags, imageUrl, projectUrl }: ProjectProps) => {
+    const ref = useRef<HTMLDivElement>(null);
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["0 1", "1.33 1"],
+    });
+    const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+    const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+    return (
+        <motion.div
+            ref={ref}
+            style={{
+                scale: scaleProgress,
+                opacity: opacityProgress,
+            }}
+            className="group mb-3  sm:mb-8 last:mb-0  w-full flex"
+        >
+            <section className=" group-even:pl-8 rounded-lg px-12 flex  sm:max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]  sm:even:pl-8 hover:bg-gray-200 transition bg-zinc-950 hover:bg-zinc-900/50 border-emerald-900">
+                <a href={projectUrl} target="_blank">
+                    <div className="pt-4 pb-7  sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col justify-center h-full sm:group-even:ml-[18rem]">
+                        <h3 className="text-2xl font-semibold text-zinc-400 group-hover:text-zinc-100">{title}</h3>
+                        <p className="mt-2 mb-8 leading-relaxed  text-zinc-400 group-hover:text-zinc-100">
+                            {description}
+                        </p>
+                    </div>
 
-          <Image
-            src={imageUrl}
-            alt='Project I worked on'
-            quality={95}
-            className=' sm:absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl 
+                    <Image
+                        src={imageUrl}
+                        alt="Project I worked on"
+                        quality={95}
+                        className=" sm:absolute top-8 -right-40 w-[28.25rem] rounded-lg z-20
+                        
             transition
             group-hover:scale-[1.04]
             group-hover:-translate-x-3
@@ -66,12 +50,26 @@ const Project = ({
     
             group-even:right-[initial] 
             group-even:-left-40 
-          '
-          />
-        </a>
-      </section>
-    </motion.div>
-  );
+          "
+                    />
+                </a>
+            </section>
+            <div className="relative h-full transition-all duration-300 -z-10 opacity-0 group-hover:opacity-1 group-hover:z-10 group-hover:translate-x-3 group-hover:opacity-100 ">
+                <h3 className="text-[0.7rem] uppercase tracking-wider text-white mb-2 font-bold">Tech stack</h3>
+                <ul className="flex flex-col  mt-4 gap-2 sm:mt-auto ">
+                    {tags.map((tag, index) => (
+                        <li
+                            className="bg-black/[0.7] px-3 text-[0.7rem] uppercase tracking-wider text-white rounded-full border border-emerald-900"
+                            key={index}
+                        >
+                            {" "}
+                            {tag}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </motion.div>
+    );
 };
 
 export default Project;
--- lib/data.ts
+++ lib/data.ts
@@ -10,94 +10,94 @@ import taskManager from "@/public/task-manager.png";
 import finace from "@/public/finance.png";
 
 export const links = [
-  {
-    name: "Home",
-    hash: "#home",
-  },
-  {
-    name: "About",
-    hash: "#about",
-  },
-  {
-    name: "Projects",
-    hash: "#projects",
-  },
-  {
-    name: "Skills",
-    hash: "#skills",
-  },
-  {
-    name: "Experience",
-    hash: "#experience",
-  },
-  {
-    name: "Contact",
-    hash: "#contact",
-  },
+    {
+        name: "Home",
+        hash: "#home",
+    },
+    {
+        name: "About",
+        hash: "#about",
+    },
+    {
+        name: "Projects",
+        hash: "#projects",
+    },
+    {
+        name: "Skills",
+        hash: "#skills",
+    },
+    {
+        name: "Experience",
+        hash: "#experience",
+    },
+    {
+        name: "Contact",
+        hash: "#contact",
+    },
 ] as const;
 
 export const experiencesData = [
-  {
-    title: "Freelance React/JavaScript Developer",
-    location: "",
-    description:
-      "Since leaving C9 Group, I have been working as a JavaScript freelance developer and I am open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "Present",
-  },
-  {
-    title: "Software Developer - C9 Group",
-    location: "Novi Sad",
-    description:
-      "After a brief internship, I joined the C9 Group/WowYowAI team as a full-stack software developer. My tech stack included React, Bootstrap, Redux, MySQL, and AWS.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2023",
-  },
-  {
-    title: "Self-Study Period",
-    location: "",
-    description:
-      "During this time, I delved into both frontend and backend technologies, learning core concepts and the MERN stack. Despite applying for internships, I faced challenges as a self-taught developer due to limited opportunities.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2020 - 2022",
-  },
+    {
+        title: "Freelance React/JavaScript Developer",
+        location: "",
+        description:
+            "Since leaving C9 Group, I have been working as a JavaScript freelance developer and I am open to full-time opportunities.",
+        icon: React.createElement(FaReact),
+        date: "Present",
+    },
+    {
+        title: "Software Developer - C9 Group",
+        location: "Novi Sad",
+        description:
+            "After a brief internship, I joined the C9 Group/WowYowAI team as a full-stack software developer. My tech stack included React, Bootstrap, Redux, MySQL, and AWS.",
+        icon: React.createElement(CgWorkAlt),
+        date: "2023",
+    },
+    {
+        title: "Self-Study Period",
+        location: "",
+        description:
+            "During this time, I delved into both frontend and backend technologies, learning core concepts and the MERN stack. Despite applying for internships, I faced challenges as a self-taught developer due to limited opportunities.",
+        icon: React.createElement(LuGraduationCap),
+        date: "2020 - 2022",
+    },
 ] as const;
 
 export const projectsData = [
-  {
-    title: "Finance Dashboard",
-    description:
-      "Fullstack dashboard for managing finances, with features for budgeting, expense tracking, and financial reporting.",
-    tags: [
-      "React / Vite",
-      "Tailwind",
-      "Typescript",
-      "Zustand / React Qeury",
-
-      "Prisma",
-      "PostgreSQL",
-      "Node.js / Express",
-    ],
-    imageUrl: finace,
-    projectUrl: "https://finance-dashboard-psi-sand.vercel.app/",
-  },
-  {
-    title: "Audiophile E-Commerce",
-    description:
-      "An online store for audiophile products, showcasing a variety of high-quality audio equipment.",
-    tags: ["Next.js", "Tailwind", "Typescript", "Redux"],
-    imageUrl: eCommerce,
-    projectUrl: "https://stalwart-bombolone-4bcc51.netlify.app",
-  },
-  {
-    title: "Designo",
-    description:
-      "This project from Frontend Mentor.io showcases my attempt at creating a pixel-perfect website.",
-    tags: ["React", "Tailwind", "Typescript"],
-    imageUrl: designo,
-    projectUrl: "https://project-designo.netlify.app/",
-  },
-  /*
+    {
+        title: "Finance Dashboard",
+        description:
+            "Fullstack dashboard for managing finances, with features for budgeting, expense tracking, and financial reporting.",
+        tags: [
+            "React",
+            "Vite",
+            "Tailwind",
+            "Typescript",
+            "Zustand ",
+            "React Qeury",
+            "Prisma",
+            "PostgreSQL",
+            "Node.js",
+            "Express",
+        ],
+        imageUrl: finace,
+        projectUrl: "https://finance-dashboard-psi-sand.vercel.app/",
+    },
+    {
+        title: "Audiophile E-Commerce",
+        description: "An online store for audiophile products, showcasing a variety of high-quality audio equipment.",
+        tags: ["Next.js", "Tailwind", "Typescript", "Redux"],
+        imageUrl: eCommerce,
+        projectUrl: "https://stalwart-bombolone-4bcc51.netlify.app",
+    },
+    {
+        title: "Designo",
+        description: "This project from Frontend Mentor.io showcases my attempt at creating a pixel-perfect website.",
+        tags: ["React", "Tailwind", "Typescript"],
+        imageUrl: designo,
+        projectUrl: "https://project-designo.netlify.app/",
+    },
+    /*
     {
         title: "Task Dashboard Full-stack project",
         description:
@@ -119,22 +119,22 @@ export const projectsData = [
 ] as const;
 
 export const skillsData = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "TypeScript",
-  "React",
-  "Next.js",
-  "Node.js",
-  "Git",
-  "Tailwind",
-  "MySQL",
-  "Redux",
-  "REST",
-  "Express",
-  "PostgresSQL",
-  "Framer Motion",
-  "Figma",
-  "Adobe Photoshop",
-  "UI/UX",
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "TypeScript",
+    "React",
+    "Next.js",
+    "Node.js",
+    "Git",
+    "Tailwind",
+    "MySQL",
+    "Redux",
+    "REST",
+    "Express",
+    "PostgresSQL",
+    "Framer Motion",
+    "Figma",
+    "Adobe Photoshop",
+    "UI/UX",
 ] as const;
